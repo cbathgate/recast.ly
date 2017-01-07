c068619ce97dcf1b7839ebc5c54a46edfe9bc0b7
cbathgate <c.bathgate1@gmail.com>
2017-01-06 20:18:52 -0800

    implement searchYoutube function and added modules to index.html

--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -1,5 +1,20 @@
 var searchYouTube = (options, callback) => {
   // TODO
+  console.log(`https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=${options.max}&q=${options.query}&key=${options.key}&type=video&videoembeddable=true`);
+  return $.ajax({
+    type: 'GET',
+    url: `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=${options.max}&q=${options.query}&key=${options.key}&type=video&videoembeddable=true`,
+    contentType: 'application/json',
+    success: function(results) {
+      console.log(results);
+      callback(results.items);
+    },
+    error: function(error, response) {
+      console.error('This is an error', error);
+      console.log(url);
+    },
+
+  }); 
 };
 
 window.searchYouTube = searchYouTube;
